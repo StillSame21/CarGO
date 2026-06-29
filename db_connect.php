65c23caf7926e91d2904873ac1b83540c8614d30
HateemNazamid21 <hateemnaza@gmail.com>
2026-06-30 02:23:17 +0800

    get db connect bug,  header block bug

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,5 @@
 <?php
+
 // db_connect.php
 
 // Enable Composer autoloader for dynamic environment handling
@@ -10,23 +11,25 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     }
 }
 
-function getDbConnection(): mysqli
-{
-    static $conn = null;
-    if ($conn !== null) {
+if (!function_exists('getDbConnection')) {
+    function getDbConnection(): mysqli
+    {
+        static $conn = null;
+        if ($conn !== null) {
+            return $conn;
+        }
+
+        $host = $_ENV['DB_HOST'] ?? '127.0.0.1';
+        $username = $_ENV['DB_USER'] ?? 'root';
+        $password = $_ENV['DB_PASS'] ?? '';
+        $database = $_ENV['DB_NAME'] ?? 'cargo_rental';
+        $port = (int)($_ENV['DB_PORT'] ?? 3306);
+
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+        $conn = new mysqli($host, $username, $password, $database, $port);
+        $conn->set_charset('utf8mb4');
+
         return $conn;
     }
-
-    $host = $_ENV['DB_HOST'] ?? '127.0.0.1';
-    $username = $_ENV['DB_USER'] ?? 'root';
-    $password = $_ENV['DB_PASS'] ?? '';
-    $database = $_ENV['DB_NAME'] ?? 'cargo_rental';
-    $port = (int)($_ENV['DB_PORT'] ?? 3306);
-
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-    $conn = new mysqli($host, $username, $password, $database, $port);
-    $conn->set_charset('utf8mb4');
-
-    return $conn;
 }
