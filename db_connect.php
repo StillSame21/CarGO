040b140e8f89fbf45d2e762b578dc229dedf3545
HateemNazamid21 <hateemnaza@gmail.com>
2026-07-22 02:58:14 +0800

    minor changes

--- db_connect.php
+++ db_connect.php
@@ -10,15 +10,6 @@ if (!function_exists('getDbConnection')) {
             return $conn;
         }
 
-        // Enable Composer autoloader for dynamic environment handling
-        if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-            require_once __DIR__ . '/vendor/autoload.php';
-            if (file_exists(__DIR__ . '/.env')) {
-                $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-                $dotenv->safeLoad();
-            }
-        }
-
         $host = $_ENV['DB_HOST'] ?? '127.0.0.1';
         $username = $_ENV['DB_USER'] ?? 'root';
         $password = $_ENV['DB_PASS'] ?? '';
