a3c7856b346a59e5eee4a935ec5f64bc78c33c35
HateemNazamid21 <hateemnaza@gmail.com>
2026-07-25 03:20:37 +0800

    update design

--- includes/layout_top.php
+++ includes/layout_top.php
@@ -41,7 +41,7 @@ $cssPrefix = ($isAdmin || $isCustomer) ? '../' : './';
         <link rel="stylesheet" href="<?php echo $cssPrefix; ?>css/admin.css?v=3">
         <link rel="stylesheet" href="<?php echo $cssPrefix; ?>css/theme.css?v=7">
     <?php elseif ($isCustomer) : ?>
-        <link rel="stylesheet" href="<?php echo $cssPrefix; ?>css/customer.css?v=8">
+        <link rel="stylesheet" href="<?php echo $cssPrefix; ?>css/customer.css?v=9">
     <?php endif; ?>
 </head>
 <body class="<?php echo $isCustomer ? 'is-customer' : ($isAdmin ? 'is-admin' : ''); ?>">
